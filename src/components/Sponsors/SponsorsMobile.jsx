b13dc0711785ea0c8a3afb69d2398afef1e6c7fa
Sumant.xD <65810424+SumantxD@users.noreply.github.com>
2022-07-29 12:08:52 +0530

    updated SponsorsMobile

--- src/components/Sponsors/SponsorsMobile.jsx
+++ src/components/Sponsors/SponsorsMobile.jsx
@@ -4,8 +4,8 @@ function SponsorsMobile() {
   return (
     <div className='flex-col bg-black'>
         <h1 className='text-3xl  font-semibold pt-[35px] ml-[30px] text-white'>Sponsors</h1>
-        <Parts text={"Platinum Sponsors"}></Parts>
-        <Parts text={"Gold Sponsors"}></Parts>
+        <Parts2 text={"Platinum Sponsors"}></Parts2>
+        <Parts2 text={"Gold Sponsors"}></Parts2>
         <Parts text={"Silver Sponsors"}></Parts>
         <Parts text={"Bronze Sponsors"}></Parts>
         <Parts text={"Partners"}></Parts>
@@ -25,5 +25,18 @@ function Parts({text}){
         </div>
         </div>
     )
+}
+function Parts2({text}){
+  return(
+      <div>
+      <div style={{backgroundImage: `linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.5) 50%, rgba(255, 255, 255, 0) 100%)`}} className='relative rounded-md h-[5px] w-[100%] mt-[35px]'></div>
+      <p className='text-white text-2xl text-center mt-1 pb-[15px] '>{text}</p>
+      <div className=' grid grid-cols-2 text-center mx-[17vw] gap-[10px] justify-center items-center pb-[40px]'>
+          <div className='h-[19vh] border-[3px] rounded-3xl text-white '></div>
+          <div className='h-[19vh] border-[3px] rounded-3xl text-white '></div>
+          
+      </div>
+      </div>
+  )
 }
 export default SponsorsMobile
